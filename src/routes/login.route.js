336bbde8c1d69ff517ca905cbe3fbda85f5d1012
Pablo Daniel Ortega <PabloOrtega145@hotmail.com>
2022-04-26 16:16:21 -0300

    Bug fixed - Login dont require token

--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -3,6 +3,6 @@ const userController = require('../controllers/user.controller.js')
 const validateToken = require('../middleware/ValidateToken')
 const login = Router()
 
-login.post('/', validateToken, userController.userLogin)
+login.post('/', userController.userLogin)
 
 module.exports = login
