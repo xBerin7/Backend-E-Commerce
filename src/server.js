8abf1b5102db313cb7b52497cabac2feb20d0086
Pablo Daniel Ortega <PabloOrtega145@hotmail.com>
2022-04-27 16:56:54 -0300

    JWT is not required for new users

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ server.name = 'API-ECOMM'
 server.use(json({ limit: '50mb' }))
 server.use(morgan('dev'))
 server.use('/', require('./routes/index.route.js'))
-server.use('/user/:id', ValidateToken, require('./routes/index.route.js'))
+server.use('/user/:id',require('./routes/index.route.js'))
 
 // Endware
 server.use((err, req, res, next) => {
