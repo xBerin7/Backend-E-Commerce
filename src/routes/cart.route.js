8fa0c835b27447ee1579b7e299163394d6e32cc0
Pablo Daniel Ortega <PabloOrtega145@hotmail.com>
2022-05-02 19:51:14 -0300

    DeleteProducts route add

--- src/routes/cart.route.js
+++ src/routes/cart.route.js
@@ -3,5 +3,6 @@ const cartController = require('../controllers/cart.controller.js')
 
 cart.get('/', cartController.getCart)
 cart.post('/add',cartController.updateCart)
+cart.post('/delete',cartController.deleteProductCart)
 
 module.exports = cart
