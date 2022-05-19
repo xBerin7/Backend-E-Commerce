1db20d37365988a045057167ec13981dfa14720f
Pablo Daniel Ortega <PabloOrtega145@hotmail.com>
2022-05-18 23:18:13 -0300

    Cart GET is POST lmao

--- src/routes/cart.route.js
+++ src/routes/cart.route.js
@@ -3,7 +3,7 @@ const cartController = require('../controllers/cart.controller.js')
 const ValidateToken = require('../middleware/ValidateToken')
 
 
-cart.get('/', ValidateToken,cartController.getCart)
+cart.post('/', ValidateToken,cartController.getCart)
 cart.post('/create',ValidateToken,cartController.createCart)
 cart.post('/add',ValidateToken,cartController.updateCart)
 cart.post('/delete',ValidateToken,cartController.deleteProductCart)
