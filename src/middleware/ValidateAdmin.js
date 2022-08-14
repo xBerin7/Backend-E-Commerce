f4d908cb37f054bbd4058b82868efcf29d809e79
Pablo Daniel Ortega <PabloOrtega145@hotmail.com>
2022-08-14 18:08:29 -0300

    Verification admin

--- src/middleware/ValidateAdmin.js
+++ src/middleware/ValidateAdmin.js
@@ -4,6 +4,7 @@ const verifyAdmin=async(req,res,next)=>{
     const admin = req.header('auth-admin')
     if(!admin)return res.status(401).json({error:true,message:"Datos incorrecto"})
     const adminVerify = await User.findById({_id:admin})
+    if(!adminVerify)return res.json({message:"No tienes acceso a esta funcion"})
     if(adminVerify.rol==false)return res.status(401).json({error:true,message:"Datos incorrecto"})
     console.log(admin)
     next()
